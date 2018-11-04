a15a0911353d61e3faade3d2f61a3e444e5b9e2e
Luke Roberts <lukeroberts@Lukes-MacBook-Pro.local>
2018-11-04 07:53:55 -0700

    initial project setup

---
+++ notes.js
@@ -0,0 +1,25 @@
+/* 
+Website Reqs:
+- STATIC
+    - SENTIMENTAL
+        - Our Story
+        - Our Wedding Philo
+    - PRACTICAL
+        - Accomidations
+        - Sight Seeing While Around
+        - Venue Info
+        - Day Of Info
+        - How to Get There
+        - Registry
+        - Contact
+        - Photos to come.
+ - 
+- DYNAMIC
+    - Registry
+    - RSVP
+
+
+
+
+*/
+
---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+require('angular');
+var myApp = angular.module('myApp',[]);
+myApp.controller('UsrCtrl', ['$scope', function ($scope) {
+    $scope.users = [
+        {
+            "name": "steve",
+            "route": "a",
+            "value": "100"
+        },
+        {
+            "name": "john",
+            "route": "a",
+            "value": "1000"
+        },
+        {
+            "name": "fransisco",
+            "route": "b",
+            "value": "10"
+        },
+        {
+            "name": "lavon",
+            "route": "c",
+            "value": "1"
+        }
+    ];
+}]);
+
+myApp.directive('customButton', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        template:   '<a href="" class="myawesomebutton" ng-transclude>' +
+                        '<i class="icon-ok-sign"></i>' +
+                    '</a>',
+        link: function (scope, element, attrs) {
+        }
+    };
+})
