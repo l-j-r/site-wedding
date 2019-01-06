2cb05fc017b9c36a676b1f516d8bb0e244a2fa1d
Luke Roberts <luke@melismatic.io>
2019-01-06 08:26:54 -0700

    make state change generic

--- src/js/controllers/WedsiteController.js
+++ src/js/controllers/WedsiteController.js
@@ -2,6 +2,9 @@ angular.module('WedsiteController', [])
 .controller('WedCtrl', WedCtrl);
 
 function WedCtrl(
+    $rootScope,
     $scope
 ) { 
+    $scope.goto = $rootScope.goto;
+    $scope.log = $rootScope.log;
 }
